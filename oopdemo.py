53edce6533fc520ee81d27bdac12b457dc2974af
Stefan Holmberg <stefan.holmberg@systementor.se>
2021-09-28 14:17:59 +0200

    oop konto

--- oopdemo.py
+++ oopdemo.py
@@ -1,3 +1,46 @@
+class Konto:
+    def __init__(self):
+        self.Kontonummer = ""
+        self.Saldo = 0
+
+kontoLista = []
+
+
+while True:
+    print("1. Skapa konto")
+    print("2. Lista konton")
+    print("4. Ändra saldo")
+    print("3. Exit")
+    sel = input()
+    if sel == "3":
+        break
+    if sel == "1":
+        kontonr = input("Ange kontonummer:")
+        nyaKontot = Konto()
+        nyaKontot.Kontonummer = kontonr
+        kontoLista.append(nyaKontot)
+    if sel == "2":
+        for konto in kontoLista:
+            print(f"{konto.Kontonummer} - {konto.Saldo}")
+
+    if sel == "4":
+        kontonr = input("Ange kontonummer på konto som du vill ändra:")
+        foundAccount = False
+        for konto in kontoLista:
+            if konto.Kontonummer == kontonr:
+                nyttSaldo = float(input("Nytt saldo"))
+                konto.Saldo = nyttSaldo
+                foundAccount = True
+                break
+        if foundAccount == False:                
+            print("Finns inte...")
+                         
+        
+
+
+
+
+
 class Person:
     def __init__(self):
         self.Namn = ""
